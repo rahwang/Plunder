0e2ae4f6a1994936c8f9d0298de67324d2213119
ngrayson <intercomman14@gmail.com>
2019-04-20 22:41:14 -0700

    started work on state manager

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -5,10 +5,31 @@ using UnityEngine;
 public class StateManager : MonoBehaviour
 {
     enum GameState {Play, Death, Win, Title};
+    GameState currentState;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    void Play(){
+        // set scene to shiplevel
+        // play music looping?
+        currentState = GameState.Play;
+    }
+
+    void Win(){
+        currentState = GameState.Win;
+        // play victory theme
+    }
+
+    void Death(){
+        currentState = GameState.Death;
+        // play death theme
+    }
+
+    void Title(){
+        currentState = GameState.Title;
     }
 
     // Update is called once per frame
