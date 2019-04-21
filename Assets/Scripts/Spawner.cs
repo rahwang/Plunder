f0c7753a93aacb957b202ecfa1d0a7acbe93644e
ngrayson <intercomman14@gmail.com>
2019-04-20 22:31:46 -0700

    idk anymore tbh

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject sailorPrefab;
     void Start()
     {
-        
+        SpawnSailor();
+    }
+
+    void SpawnSailor(){
+        float x = this.transform.position.x;
+        float y = this.transform.position.y;
+        float z = this.transform.position.z;
+
+        y += (float)0.3;
+
+        Instantiate(sailorPrefab, new Vector3(x, y, z), Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( 3 == Random.Range(0,8000))
+        {
+            // SpawnSailor();
+        }
+
     }
 }
