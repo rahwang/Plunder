8e05347472f9f5cf0d1382405594079f94b6837c
pastasfuture <pastasfuture@gmail.com>
2019-04-21 13:11:55 -0700

    Spice controller

---
+++ Assets/Scripts/SpiceController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiceController : MonoBehaviour
+{
+    private static SpiceController instance = null;
+
+    [Tooltip("1 == most reponsive, 0.0 == most dampening"), Range(0.0f, 1.0f)]
+    public float spice = 1.0f;
+
+    void Awake()
+    {
+        Debug.Assert(SpiceController.instance == null);
+        SpiceController.instance = this;
+    }
+
+    public static float GetSpice()
+    {
+        Debug.Assert(SpiceController.instance != null);
+        return SpiceController.instance.spice;
+    }
+}
